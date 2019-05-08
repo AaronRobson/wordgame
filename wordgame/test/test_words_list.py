2bdab52e14f34668eb57502120c5876c2afbc61c
Aaron Robson <aaron.robson@citrix.com>
2019-05-08 21:28:48 +0100

    unittests of the contents of the words.txt file

---
+++ wordgame/test/test_words_list.py
@@ -0,0 +1,25 @@
+import unittest
+
+from wordgame import _find_list_of_words
+
+
+class TestWordsList(unittest.TestCase):
+    '''Tests that the contents of the words.txt file are valid'''
+    @classmethod
+    def setUpClass(cls):
+        cls.words = list(_find_list_of_words())
+
+    def test_no_empty_string_words(self):
+        for word in self.words:
+            self.assertNotEqual(len(word), 0)
+
+    def test_no_whitespace_are_present(self):
+        for word in self.words:
+            for character in word:
+                self.assertFalse(
+                    character.isspace(),
+                    f'{word} should not have any whitespace characters')
+
+    def test_all_words_are_alphabetical(self):
+        for word in self.words:
+            self.assertTrue(word.isalpha())
